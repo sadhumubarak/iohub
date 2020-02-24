07c65a6d2bdb8c946492377a6bd7efcb6a5dc2a4
sadhumubarak <smshussain007@gmail.com>
2020-02-24 16:23:22 +0530

    temporary

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,13 +1,5 @@
 import React, { Component, Fragment } from 'react';
 
-import Header from '../Header';
-import Work from '../Work';
-import Gallery from '../Gallery';
-import Subscribe from '../Subscribe';
-import Article from '../Article';
-import About from '../About';
-import Footer from '../Footer';
-
 import './index.scss';
 
 class Home extends Component {
@@ -24,28 +16,10 @@ class Home extends Component {
             <Fragment>
                 <Header />
                 <div className="container-home">
-                <div className="showcase" id="home" data-aos="fade-up">
-                    <div className="showcase-box-1">
-                        <h1 className="showcase-title">Product Designer</h1>
-                        <p className="showcase-paragraph">
-                            We design thoughtful experiences to help you gain competitive advantage and grow your business event further
-                        </p>
-                        <button className="btn-start">
-                            Get started
-                        </button>
-                    </div>
-                    <div className="showcase-box-2">
-                        <img src="./img-1.jpg" alt="illustrator-file" />
-                    </div>
-                </div>
+                akjsdflk
                 </div>
 
-                <Work/>
-                <Gallery/>
-                <Subscribe/>
-                <Article/>
-                <About/>
-                <Footer/>
+                
             </Fragment>
         );
     }
